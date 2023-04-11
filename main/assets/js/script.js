6d66520214ff41f7d38ba02533331fd164b86e42
nsrsh25 <108688245+nsrsh25@users.noreply.github.com>
2023-04-11 10:16:32 +0430

    change names

--- main/assets/js/script.js
+++ main/assets/js/script.js
@@ -1,8 +1,8 @@
 // ----------select elements------------
 const form = document.querySelector("#form"),
-  selectModel = document.querySelector("#select-model"),
-  selectYear = document.querySelector("#select-year"),
-  radioChecked = document.querySelector("input[type=radio]:checked"),
+  model = document.querySelector("#select-model"),
+  year = document.querySelector("#select-year"),
+  type = document.querySelector("input[type=radio]:checked"),
   result = document.querySelector("#result");
 // ----------functions---------------
 // adding images to each option
@@ -15,13 +15,11 @@ function changeImage(element) {
 function dateConvertor() {
   return (shamsiDate = new Date().getFullYear() - 621);
 }
-// console.log(dateConvertor());
 
 // tolide yek range az adad ba dadane adade ebtdaei va tedad(tool)
 function createRange(start, length) {
   return Array.from({ length: length + 1 }, (_, i) => start - i);
 }
-// console.log(createRange(1402, 20));
 
 // Function insertDate
 // darje optionhaye marbot be sale saakhte mashin dar safhe
