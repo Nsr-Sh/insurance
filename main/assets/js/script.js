71c32a88f757883266f2e9900078534ca6b143f8
nsrsh25 <108688245+nsrsh25@users.noreply.github.com>
2023-04-12 14:30:09 +0430

    add class

--- main/assets/js/script.js
+++ main/assets/js/script.js
@@ -1,9 +1,12 @@
 // ----------select elements------------
 const form = document.querySelector("#form"),
-  model = document.querySelector("#select-model"),
-  year = document.querySelector("#select-year"),
-  type = document.querySelector("input[type=radio]:checked"),
-  result = document.querySelector("#result");
+  selectModel = document.querySelector("#select-model"),
+  selectYear = document.querySelector("#select-year"),
+  userCar = document.querySelector("#user-car"),
+  userYear = document.querySelector("#user-year"),
+  userType = document.querySelector("#user-type"),
+  cost = document.querySelector("#cost");
+
 // ----------functions---------------
 // adding images to each option
 function changeImage(element) {
@@ -33,9 +36,83 @@ function insertDate(length) {
   rangeArray.forEach((item) => {
     html += `<option value="${item}">${item}</option>`;
   });
-  year.insertAdjacentHTML("beforeEnd", html);
+  selectYear.insertAdjacentHTML("beforeEnd", html);
 }
 
 insertDate(20);
 
 // ----------class-------------------
+// Class Factor:this class creates a insurance factor
+// object based on input parameter
+// input:car model, car year ,insurance type
+class Factor {
+  constructor(model, year, type) {
+    this.model = model;
+    this.year = year;
+    this.type = type;
+    this.price = 3000;
+    this.yearIncrease();
+    this.typeIncrease();
+    this.modelIncrease();
+  }
+
+  yearIncrease(val = 2) {
+    const diff = 1402 - this.year;
+    return (this.price = this.price + (this.price * diff * val) / 100);
+  }
+
+  typeIncrease(s = 20, c = 35) {
+    this.type === "Simple"
+      ? (this.price = this.price + (this.price * s) / 100)
+      : (this.price = this.price + (this.price * c) / 100);
+    return this.price;
+  }
+
+  modelIncrease() {
+    switch (this.model) {
+      case "BMW":
+        this.price = this.price + (this.price * 30) / 100;
+        break;
+      case "lamborghini":
+        this.price = this.price + (this.price * 25) / 100;
+        break;
+      case "pourshe":
+        this.price = this.price + (this.price * 40) / 100;
+        break;
+      case "pride":
+        this.price = this.price + (this.price * 10) / 100;
+        break;
+      default:
+        this.price = this.price + (this.price * 20) / 100;
+    }
+    return this.price;
+  }
+}
+// const summery = new Factor("pourshe", 1400, "simple");
+// ------------event-----------------
+// event submit form: get inputs from user
+//  make new insurance object and call showFactor function
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const year = selectYear.value,
+    model = selectModel.value,
+    type = document.querySelector("input:checked+label").textContent;
+  console.log(year, model, type);
+  const factor = new Factor(model, year, type);
+  console.log(factor);
+  showFactor(factor);
+});
+
+// -------------------------------
+// Function showFactor:display result
+function showFactor(factor) {
+  userCar.textContent = `✔ Car : ${factor.model}`;
+  userYear.textContent = `✔ Year : ${factor.year}`;
+  userType.textContent = `✔ Type : ${factor.type}`;
+  cost.textContent = `${Math.round(factor.price)} $`;
+  result.style.display = "flex";
+  const timeFun = setTimeout(function () {
+    result.style.opacity = 1;
+  }, 200);
+}
